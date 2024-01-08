84da42b2ca3561e41f2e741ae06f0720886b16ee
needf <needforspeedun3@gmail.com>
2024-01-09 01:27:46 +0200

    last commit 2023 8/1 create file of images

---
+++ lib/practical_project/utlis/app_images.dart
@@ -0,0 +1,55 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAvatarOne
+  /// assetss/images/avatar_one.svg
+  static const String imagesAvatarOne = "assetss/images/avatar_one.svg";
+
+  /// Assets for imagesAvatarTow
+  /// assetss/images/avatar_tow.svg
+  static const String imagesAvatarTow = "assetss/images/avatar_tow.svg";
+
+  /// Assets for imagesBalance
+  /// assetss/images/balance.svg
+  static const String imagesBalance = "assetss/images/balance.svg";
+
+  /// Assets for imagesCard
+  /// assetss/images/card.svg
+  static const String imagesCard = "assetss/images/card.svg";
+
+  /// Assets for imagesDashboard
+  /// assetss/images/dashboard.svg
+  static const String imagesDashboard = "assetss/images/dashboard.svg";
+
+  /// Assets for imagesExpenses
+  /// assetss/images/expenses.svg
+  static const String imagesExpenses = "assetss/images/expenses.svg";
+
+  /// Assets for imagesIncome
+  /// assetss/images/income.svg
+  static const String imagesIncome = "assetss/images/income.svg";
+
+  /// Assets for imagesInvestments
+  /// assetss/images/investments.svg
+  static const String imagesInvestments = "assetss/images/investments.svg";
+
+  /// Assets for imagesLogout
+  /// assetss/images/logout.svg
+  static const String imagesLogout = "assetss/images/logout.svg";
+
+  /// Assets for imagesSetting
+  /// assetss/images/setting.svg
+  static const String imagesSetting = "assetss/images/setting.svg";
+
+  /// Assets for imagesStatistics
+  /// assetss/images/statistics.svg
+  static const String imagesStatistics = "assetss/images/statistics.svg";
+
+  /// Assets for imagesTransaction
+  /// assetss/images/transaction.svg
+  static const String imagesTransaction = "assetss/images/transaction.svg";
+
+  /// Assets for imagesWalletAccount
+  /// assetss/images/wallet_account.svg
+  static const String imagesWalletAccount = "assetss/images/wallet_account.svg";
+}
